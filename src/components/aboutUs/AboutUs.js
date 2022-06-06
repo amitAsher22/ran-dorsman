06e86bbda5e65172aec9c82daed2fcb4750f7760
amita <amitasher4@gmail.com>
2022-06-06 21:57:50 +0300

    finisf carousel

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,4 +1,6 @@
+
 import React , { useState , useEffect } from 'react'
+import { LngLatBounds } from 'react-map-gl';
 import '../../css/aboutUs.css'
 // import FsLightbox from 'fslightbox-react';
 // import Lightbox from "fslightbox-react";
@@ -13,42 +15,47 @@ function AboutUs() {
 
   const [show, setShow] = useState(false);
   const [oneCoach, setoneCoach] = useState({});
-  const [number, setNumber] = useState();
+  const [number, setNumber] = useState(0);
+  const [disable, setdisable] = useState(false);
+ 
+ 
   const [coaches, setCoaches] = useState([
     {
-      imageCoach:
-        "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748",
+      imageCoach: "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748",
       name: "רן דורסמן",
       text: "אורלי למדה הדרכת עיצוב וחיטוב בוינגייט לפני כעשור וכעת מסיימת לימודי הדרכת פילאטיס מזרן",
       id: 0,
     },
     {
-      imageCoach:
-        "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442",
+      imageCoach: "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442",
+       
 
       name: "אליק",
       text: "שחקנית יוצרת ומחזאית. עוסקת בהוראה ובעלת תואר בבימוי והוראה מטעם סמינר הקיבוצים ",
       id: 1,
     },
     {
-      imageCoach:
-        "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fe.jpg?v=1560731959643",
+      imageCoach: "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fe.jpg?v=1560731959643",
+       
       name: "עמית",
       text: "  התשוקה שלי היא תנועה וריקו בנוסף יש לי קליניקה לפדיקורפלקסולוגיה - טיפול פדיקור ",
       id: 2,
     },
   ]);
+  
 
   useEffect(()=>{
-    setoneCoach(coaches[number])
+    
+    setoneCoach(coaches[number]) //// 2
+
   },[number])
 
   const specificCoache = (num) => {
     setNumber(num);
-    console.log(num);
-    const oneCoach = coaches.find((element) => element.id === num);
+    const oneCoach = coaches.find((element) => element.id === num); /// {id , img , text}
     setoneCoach(oneCoach);
     setShow(true);
+    // setdisable(oneCoach.id === coaches[coaches.length - 1].id || oneCoach.id ===  0 ? true : false)
   };
 
   return (
@@ -62,27 +69,31 @@ function AboutUs() {
 
           <div className="slider-wrapper">
             <section className="left">
-              
-           
               <button
-                onClick={() => (
-                
-                  setNumber(number + 1),
-                  setoneCoach(coaches[number])
-                )}
+                onClick={() => 
+                  setNumber(number => number >= coaches.length -1 ? 0 : number + 1 )
+                  // setoneCoach(coaches[number])
+                 
+                  // oneCoach.id === coaches[coaches.length -1] ? console.log('work') : console.log('no work')
+       
+                }
+           
               >
                 ⬅
               </button>
               <img src={oneCoach.imageCoach} alt={oneCoach.name} />
-                <button
-                  onClick={() => (
-                    setNumber(number - 1 ),
-                    console.log(number),
-                    setoneCoach(coaches[number])
-                  )}
+              
+               <button
+                  onClick={() => 
+                    setNumber(number => number <= 0 ? coaches.length - 1 : number - 1 )
+                  
+
+                  }
+                 
                 >
                   ➡
                 </button>
+               
              
             </section>
             <section className="right">
@@ -101,14 +112,14 @@ function AboutUs() {
               עבודה על שרירי ליבה בדופק משתנה עם משקלים נמוכים שיעור בר הוא
               שיעור דינאמי ואנרגטי ששואב השראה מבלט, פילאטיס ועבודה אירובית
               ואפילו מתבצעו וגמישות ועובר בין כל קבוצות השרירים בגובחלק משלב
-              אלמנטים של כוח וגמישות ועובר בין כל קבוצות השרירים בגוף. <p></p>ו
-              מול בר בלט נשלב עבודה על שרירי ליבה בדופק משתנה עם משקלים נמוכים{" "}
+              אלמנטים של כוח וגמישות ועובר בין כל קבוצות השרירים בגוף. 
+              מול בר בלט נשלב עבודה על שרירי ליבה בדופק משתנה עם משקלים נמוכים
             </p>
             <h1 className="titleTeam">הצוות והמורים</h1>
           </div>
           {coaches.map((coach, index) => (
-            <div onClick={() => specificCoache(index)} key={index}>
-              <img className="imgCoach" src={coach.imageCoach} />
+            <div onClick={() => specificCoache(coach.id)} key={index}>
+              <img className="imgCoach" src={coach.imageCoach} alt={coach.name}/>
               <p>{coach.name}</p>
             </div>
           ))}

241291f59d9964c0a5d7b6944b001b43b0888071
amita <amitasher4@gmail.com>
2022-06-07 14:26:21 +0300

    two page

--- src/components/TrainingGroups/TrainingGroups.js
+++ src/components/TrainingGroups/TrainingGroups.js
@@ -1,21 +1,17 @@
 import React, { useState } from "react";
 import "../../css/TrainingGroups.css";
-import Lightbox from "fslightbox-react";
+
 
 const TrainingGroups = () => {
   const [changeNum, setChangeNum] = useState(null);
   const [show, setShow] = useState(false);
+  const [images , setImages] = useState([
+    'https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748' 
+   ,'https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442'
+   ,'https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fe.jpg?v=1560731959643'
+   ,'https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fb.jpg?v=1560731962200'
+  ])
 
-  const showSlide = (num) => {
-    setChangeNum(num);
-    setShow(!show);
-  };
-  const styleImg = {
-    display: "flex",
-    // padding:"4px",
-    width: "15vw",
-    height: "10vw",
-  };
 
   return (
     <div className="backgroundGroup">
@@ -27,87 +23,20 @@ const TrainingGroups = () => {
           <button className="fileBtn">מערכת PDF מותאמת לנשים בהריון</button>
         </div>
       </div>
-      {/* ////////////////// */}
+      {/* ////////////////// group images*/}
       
-        <div className="imagesGROUPS">
-          <div className="divImgAboutUs">
-            <img
-              onClick={() => showSlide(1)}
-              style={styleImg}
-              src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748"
-              alt="swamp"
-            />
-            <h2 className="titleMan">פונקציונלי</h2>
-          </div>
-          <div className="divImgAboutUs">
-            <img
-              onClick={() => showSlide(2)}
-              style={styleImg}
-              src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442"
-              alt="swamp"
-            />
-            <h2 className="titleMan">כוח</h2>
-          </div>
-          <div className="divImgAboutUs">
-            <img
-              onClick={() => showSlide(3)}
-              style={styleImg}
-              src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fe.jpg?v=1560731959643"
-              alt="swamp"
-            />
-            <h2 className="titleMan">יוגה</h2>
-          </div>
-          <div className="divImgAboutUs">
-            <img
-              onClick={() => showSlide(4)}
-              src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fb.jpg?v=1560731962200"
-              style={styleImg}
-              alt="swamp"
-            />
-            <h2 className="titleMan">מתחיל ריצה</h2>
-          </div>
-          <div className="divImgAboutUs">
-            <img
-              onClick={() => showSlide(5)}
-              src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fa.jpg?v=1560731966020"
-              style={styleImg}
-              alt="swamp"
-            />
-            <h2 className="titleMan">פילאטיס</h2>
-          </div>
-          <div className="divImgAboutUs">
-            <img
-              onClick={() => showSlide(6)}
-              src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fa.jpg?v=1560731966020"
-              style={styleImg}
-              alt="swamp"
-            />
-            <h2 className="titleMan">ריצה</h2>
-          </div>
-          <div className="divImgAboutUs">
-            <img
-              onClick={() => showSlide(7)}
-              src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748"
-              style={styleImg}
-              alt="swamp"
-            />
-            <h2 className="titleMan">איתי</h2>
-          </div>
-
-          <Lightbox
-            toggler={show}
-            slide={changeNum}
-            sources={[
-              "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748",
-              "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442",
-              "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fe.jpg?v=1560731959643",
-              "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fb.jpg?v=1560731962200",
-              "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fa.jpg?v=1560731966020",
-              "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fa.jpg?v=1560731966020",
-              "https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748",
-            ]}
-          />
-        </div>
+      <div className="groupImagesAll">
+       <div> <img src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Fd.jpg?v=1560731955748" className="imgTraining" alt="img1"/> <p className="titleGroupImg">פונקציונלי</p></div>
+       <div> <img src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442" className="imgTraining" alt="img2"/> <p className="titleGroupImg">פונקציונלי</p></div>
+       <div> <img src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442" className="imgTraining" alt="img5"/> <p className="titleGroupImg">פונקציונלי</p></div>
+       <div> <img src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442" className="imgTraining" alt="img5"/> <p className="titleGroupImg">פונקציונלי</p></div>
+       <div> <img src="https://cdn.glitch.com/e4c45146-a1cb-43c3-86fe-35bf46516a43%2Ff.jpg?v=1560731959442" className="imgTraining" alt="img5"/> <p className="titleGroupImg">פונקציונלי</p></div>
+ 
+       
+      
+      
+      </div>
+       
      
     </div>
   );
